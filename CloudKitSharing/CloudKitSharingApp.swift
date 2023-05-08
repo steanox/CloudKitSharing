e2a47b82149a6cf5c7fa53039be0319b690d37ca
Octavianus <oktagandajaya@gmail.com>
2023-05-08 16:13:20 +0700

    Initial Commit

---
+++ CloudKitSharing/CloudKitSharingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudKitSharingApp.swift
+//  CloudKitSharing
+//
+//  Created by octavianus on 08/05/23.
+//
+
+import SwiftUI
+
+@main
+struct CloudKitSharingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
